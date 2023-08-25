2b4b442b7a3b7f49d90c9b3f27f7458996f8d47c
MrMurman <74971676+MrMurman@users.noreply.github.com>
2023-08-25 09:34:57 +0300

    Initial Commit

---
+++ Chlng-03-HabbitTracker/Chlng_03_HabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chlng_03_HabbitTrackerApp.swift
+//  Chlng-03-HabbitTracker
+//
+//  Created by Андрей Бородкин on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Chlng_03_HabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
